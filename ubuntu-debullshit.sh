ea1496dca171d55b82f63e8da8645609c2027c6a
Carlos Gabriel <oditorrinco222@gmail.com>
2025-12-23 23:07:53 -0300

    update

--- ubuntu-debullshit.sh
+++ ubuntu-debullshit.sh
@@ -1,39 +1,40 @@
 #!/usr/bin/env bash
 
 disable_ubuntu_report() {
-    ubuntu-report send no
-    apt remove ubuntu-report -y
+    ubuntu-report send no 2>/dev/null || true
+    apt remove ubuntu-report -y || true
 }
 
 remove_appcrash_popup() {
-    apt remove apport apport-gtk -y
+    apt remove apport apport-gtk -y || true
 }
 
 remove_snaps() {
-    while [ "$(snap list | wc -l)" -gt 0 ]; do
-        for snap in $(snap list | tail -n +2 | cut -d ' ' -f 1); do
-            snap remove --purge "$snap" 2> /dev/null
+    if snap list >/dev/null 2>&1; then
+        while [ "$(snap list | wc -l)" -gt 1 ]; do
+            for snap in $(snap list | tail -n +2 | awk '{print $1}'); do
+                snap remove --purge "$snap" 2>/dev/null || true
+            done
         done
-    done
-
-    systemctl stop snapd
-    systemctl disable snapd
-    systemctl mask snapd
-    apt purge snapd -y
-    rm -rf /snap /var/lib/snapd
-    for userpath in /home/*; do
-        rm -rf $userpath/snap
-    done
+    fi
+    systemctl stop snapd.socket snapd.service 2>/dev/null || true
+    systemctl disable snapd.socket snapd.service 2>/dev/null || true
+    systemctl mask snapd.socket snapd.service 2>/dev/null || true
+    apt purge snapd -y || true
+    rm -rf /snap /var/snap /var/lib/snapd /var/cache/snapd
+    rm -rf /home/*/snap
     cat <<-EOF | tee /etc/apt/preferences.d/nosnap.pref
-	Package: snapd
-	Pin: release a=*
-	Pin-Priority: -10
-	EOF
+Package: snapd
+Pin: release a=*
+Pin-Priority: -10
+EOF
 }
 
 disable_terminal_ads() {
-    sed -i 's/ENABLED=1/ENABLED=0/g' /etc/default/motd-news 2>/dev/null
-    pro config set apt_news=false
+    sed -i 's/ENABLED=1/ENABLED=0/g' /etc/default/motd-news 2>/dev/null || true
+    if command -v pro; then
+        pro config set apt_news=false 2>/dev/null || true
+    fi
 }
 
 update_system() {
@@ -41,88 +42,32 @@ update_system() {
 }
 
 cleanup() {
-    apt autoremove -y
+    apt autoremove -y && apt autoclean
 }
 
 setup_flathub() {
-    apt install flatpak -y
+    apt install flatpak gnome-software-plugin-flatpak --no-install-recommends -y
     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    apt install --install-suggests gnome-software -y
-}
-
-gsettings_wrapper() {
-    if ! command -v dbus-launch; then
-        sudo apt install dbus-x11 -y
-    fi
-    sudo -Hu $(logname) dbus-launch gsettings "$@"
-}
-
-set_fonts() {
-	gsettings_wrapper set org.gnome.desktop.interface monospace-font-name "Monospace 10"
-}
-
-setup_vanilla_gnome() {
-    apt install qgnomeplatform-qt5 -y
-    apt install qgnomeplatform-qt6 -y
-    apt install gnome-session fonts-cantarell adwaita-icon-theme gnome-backgrounds gnome-tweaks vanilla-gnome-default-settings gnome-shell-extension-manager -y && apt remove ubuntu-session yaru-theme-gnome-shell yaru-theme-gtk yaru-theme-icon yaru-theme-sound -y
-    set_fonts
-    restore_background
-}
-
-restore_background() {
-    gsettings_wrapper set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/gnome/blobs-l.svg'
-    gsettings_wrapper set org.gnome.desktop.background picture-uri-dark 'file:///usr/share/backgrounds/gnome/blobs-l.svg'
-}
-
-install_adwgtk3() {    
-    wget -O /tmp/adw-gtk3.tar.xz https://github.com/lassekongo83/adw-gtk3/releases/download/v5.10/adw-gtk3v5.10.tar.xz
-    tar -xf /tmp/adw-gtk3.tar.xz -C /usr/share/themes/
-    if command -v flatpak; then
-        flatpak install -y runtime/org.gtk.Gtk3theme.adw-gtk3-dark
-        flatpak install -y runtime/org.gtk.Gtk3theme.adw-gtk3
-    fi
-    if [ "$(gsettings_wrapper get org.gnome.desktop.interface color-scheme | tail -n 1)" == ''\''prefer-dark'\''' ]; then
-        gsettings_wrapper set org.gnome.desktop.interface gtk-theme adw-gtk3-dark
-        gsettings_wrapper set org.gnome.desktop.interface color-scheme prefer-dark
-    else
-        gsettings_wrapper set org.gnome.desktop.interface gtk-theme adw-gtk3
-    fi
-}
-
-install_icons() {
-    apt install adwaita-icon-theme -y
-    apt install git -y
-    git clone https://github.com/somepaulo/MoreWaita.git /tmp/MoreWaita
-    /tmp/MoreWaita/install.sh
-    apt install adwaita-icon-theme -y
-    gsettings_wrapper set org.gnome.desktop.interface icon-theme MoreWaita
-    gsettings_wrapper set org.gnome.desktop.interface accent-color blue
 }
 
 restore_firefox() {
-    wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- > /etc/apt/keyrings/packages.mozilla.org.asc
-    echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" > /etc/apt/sources.list.d/mozilla.list 
-    echo '
+    wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+    echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | tee /etc/apt/sources.list.d/mozilla.list
+    cat <<-EOF | tee /etc/apt/preferences.d/mozilla
 Package: *
 Pin: origin packages.mozilla.org
 Pin-Priority: 1000
-' > /etc/apt/preferences.d/mozilla
+EOF
     apt update
     apt install firefox -y
 }
 
 ask_reboot() {
-    echo 'Reboot now? (y/n)'
-    while true; do
-        read choice
-        if [[ "$choice" == 'y' || "$choice" == 'Y' ]]; then
-            reboot
-            exit 0
-        fi
-        if [[ "$choice" == 'n' || "$choice" == 'N' ]]; then
-            break
-        fi
-    done
+    echo 'Deseja reiniciar agora? (y/n)'
+    read -r choice
+    if [[ "$choice" =~ ^[Yy]$ ]]; then
+        reboot
+    fi
 }
 
 msg() {
@@ -139,134 +84,75 @@ error_msg() {
 
 check_root_user() {
     if [ "$(id -u)" != 0 ]; then
-        echo 'Please run the script as root!'
-        echo 'We need to do administrative tasks'
-        exit
+        error_msg 'Execute o script como root (sudo)!'
+        exit 1
     fi
 }
 
-enable_appindicator() {
-    gsettings_wrapper set org.gnome.shell enabled-extensions "['ubuntu-appindicators@ubuntu.com']"
-}
-
 print_banner() {
-    echo '                                                                                                                                   
-    ▐            ▗            ▐     ▐       ▝▜  ▝▜      ▐    ▝   ▗   ▗  
-▗ ▗ ▐▄▖ ▗ ▗ ▗▗▖ ▗▟▄ ▗ ▗      ▄▟  ▄▖ ▐▄▖ ▗ ▗  ▐   ▐   ▄▖ ▐▗▖ ▗▄  ▗▟▄  ▐  
-▐ ▐ ▐▘▜ ▐ ▐ ▐▘▐  ▐  ▐ ▐     ▐▘▜ ▐▘▐ ▐▘▜ ▐ ▐  ▐   ▐  ▐ ▝ ▐▘▐  ▐   ▐   ▐  
-▐ ▐ ▐ ▐ ▐ ▐ ▐ ▐  ▐  ▐ ▐  ▀▘ ▐ ▐ ▐▀▀ ▐ ▐ ▐ ▐  ▐   ▐   ▀▚ ▐ ▐  ▐   ▐   ▝  
-▝▄▜ ▐▙▛ ▝▄▜ ▐ ▐  ▝▄ ▝▄▜     ▝▙█ ▝▙▞ ▐▙▛ ▝▄▜  ▝▄  ▝▄ ▝▄▞ ▐ ▐ ▗▟▄  ▝▄  ▐  
-                                                                                                      
- By @polkaulfield
- '
+    echo '
+    Ubuntu Debloat Simples
+    By @polkaulfield (versão limpa sem temas vanilla)
+    '
 }
 
 show_menu() {
-    echo 'Choose what to do: '
-    echo '1 - Apply everything (RECOMMENDED)'
-    echo '2 - Disable Ubuntu report'
-    echo '3 - Remove app crash popup'
-    echo '4 - Remove snaps and snapd'
-    echo '5 - Disable terminal ads (LTS versions)'
-    echo '6 - Install flathub and gnome-software'
-    echo '7 - Install firefox from the Mozilla repo'
-    echo '8 - Install vanilla GNOME session'
-    echo '9 - Install adw-gtk3 and morewaita'
-    echo 'q - Exit'
+    echo 'Escolha o que fazer: '
+    echo '1 - Aplicar tudo (RECOMENDADO)'
+    echo '2 - Desativar relatório Ubuntu'
+    echo '3 - Remover popup de crash'
+    echo '4 - Remover snaps e snapd'
+    echo '5 - Desativar anúncios no terminal (LTS)'
+    echo '6 - Instalar Flathub e GNOME Software'
+    echo '7 - Instalar Firefox do repositório Mozilla'
+    echo 'q - Sair'
     echo
 }
 
 main() {
     check_root_user
     while true; do
+        clear
         print_banner
         show_menu
-        read -p 'Enter your choice: ' choice
+        read -p 'Sua escolha: ' choice
         case $choice in
         1)
             auto
-            msg 'Done!'
+            msg 'Tudo concluído!'
             ask_reboot
             ;;
-        2)
-            disable_ubuntu_report
-            msg 'Done!'
-            ;;
-        3)
-            remove_appcrash_popup
-            msg 'Done!'
-            ;;
-        4)
-            remove_snaps
-            msg 'Done!'
-            ask_reboot
-            ;;
-        5)
-            disable_terminal_ads
-            msg 'Done!'
-            ;;
-        6)
-            update_system
-            setup_flathub
-            msg 'Done!'
-            ask_reboot
-            ;;
-        7)
-            restore_firefox
-            msg 'Done!'
-            ;;
-        8)
-            update_system
-            setup_vanilla_gnome
-	    enable_appindicator
-            msg 'Done!'
-            ask_reboot
-            ;;
-
-        9)
-            update_system
-            install_adwgtk3
-            install_icons
-            msg 'Done!'
-            ask_reboot
-            ;;
-
-        q)
-            exit 0
-            ;;
-
-        *)
-            error_msg 'Wrong input!'
-            ;;
+        2) disable_ubuntu_report; msg 'Concluído!' ;;
+        3) remove_appcrash_popup; msg 'Concluído!' ;;
+        4) remove_snaps; msg 'Concluído!'; ask_reboot ;;
+        5) disable_terminal_ads; msg 'Concluído!' ;;
+        6) update_system; setup_flathub; msg 'Concluído!'; ask_reboot ;;
+        7) restore_firefox; msg 'Concluído!' ;;
+        q|Q) exit 0 ;;
+        *) error_msg 'Opção inválida!' ;;
         esac
+        echo "Pressione Enter para continuar..."
+        read
     done
-
 }
 
 auto() {
-    msg 'Updating system'
+    msg 'Atualizando sistema'
     update_system
-    msg 'Disabling ubuntu report'
+    msg 'Desativando relatório Ubuntu'
     disable_ubuntu_report
-    msg 'Removing annoying appcrash popup'
+    msg 'Removendo popup de crash'
     remove_appcrash_popup
-    msg 'Removing terminal ads (if they are enabled)'
+    msg 'Desativando anúncios no terminal'
     disable_terminal_ads
-    msg 'Deleting everything snap related'
+    msg 'Removendo snaps'
     remove_snaps
-    msg 'Setting up flathub'
+    msg 'Configurando Flathub'
     setup_flathub
-    msg 'Restoring Firefox from mozilla repository'
+    msg 'Instalando Firefox Mozilla'
     restore_firefox
-    msg 'Installing vanilla Gnome session'
-    setup_vanilla_gnome
-    enable_appindicator
-    msg 'Install adw-gtk3'
-    install_adwgtk3
-    msg 'Installing MoreWaita icons'
-    install_icons
-    msg 'Cleaning up'
+    msg 'Limpando pacotes desnecessários'
     cleanup
 }
 
-(return 2> /dev/null) || main
+(return 2>/dev/null) || main
